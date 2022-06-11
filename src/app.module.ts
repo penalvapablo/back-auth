6204d66e7ba592533c900b26543ae785b8cd1f30
Pablo Penalva <penalva.pablo@gmail.com>
2022-06-11 15:35:50 -0300

    first commit\

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://pablo:<password>@learn-node.tp5yo.mongodb.net/chatDB?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
