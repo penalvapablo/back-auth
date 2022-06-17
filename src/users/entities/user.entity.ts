5f4508643bf84bfbd1d020119b1ff07559277cb9
Nicolas Bes <nicobes14@gmail.com>
2022-06-17 03:28:03 -0300

    add name prop to user schema

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -5,6 +5,8 @@ export type UserDocument = User & Document;
 
 @Schema()
 export class User {
+  @Prop()
+  name: string;
   @Prop({ required: true, unique: true })
   email: string;
   @Prop({ required: true })
