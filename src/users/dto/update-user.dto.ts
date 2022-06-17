d2ba1a753bc71a6f82f11ea6ca73709588b9ee65
Nicolas Bes <nicobes14@gmail.com>
2022-06-17 03:28:15 -0300

    add update user dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { RegisterLoginDto } from './registe-login.dto';
-
-export class UpdateUserDto extends PartialType(RegisterLoginDto) {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+export class UpdateUserDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+  @IsString()
+  bio: string;
+  @IsString()
+  name: string;
+  @IsString()
+  photo: string;
+  @IsString()
+  phone: string;
+}
